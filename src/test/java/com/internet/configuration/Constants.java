783e7b0dbe3ced7c38b8009852fa331461b87af8
hfsharingan <huong.van.nguyen@logigear.com>
2026-09-13 22:56:05 +0700

    update

--- src/test/java/com/internet/configuration/Constants.java
+++ src/test/java/com/internet/configuration/Constants.java
@@ -3,6 +3,6 @@ package com.internet.configuration;
 import com.internet.utils.Utilities;
 
 public class Constants {
-    public static final String BASE_URL = "https://www.google.com.vn/";
-    public static String REPORT_IN_USE = Utilities.getEnv("REPORT_IN_USE", "allure"); // accepted value: 'allure' and 'extend'
+    public static final String BASE_URL = System.getProperty("baseUrl", Utilities.getEnv("BASE_URL", "https://www.google.com/"));
+    public static String REPORT_IN_USE = System.getProperty("reportInUse", Utilities.getEnv("REPORT_IN_USE", "allure")); // accepted value: 'allure' and 'extend'
 }
