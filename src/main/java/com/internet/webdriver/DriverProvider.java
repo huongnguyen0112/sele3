55e5b5aa1f97ea60852ad7973a074fe797fc82d9
hfsharingan <huong.van.nguyen@logigear.com>
2026-09-14 00:24:39 +0700

    update

--- src/main/java/com/internet/webdriver/DriverProvider.java
+++ src/main/java/com/internet/webdriver/DriverProvider.java
@@ -3,6 +3,9 @@ package com.internet.webdriver;
 import com.google.common.base.Throwables;
 import com.internet.webdriver.selenium.AbstractDriverProvider;
 import org.openqa.selenium.WebDriver;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 
 import java.lang.reflect.Constructor;
 
@@ -11,21 +14,24 @@ public class DriverProvider {
 
     static AbstractDriverProvider newInstance(DriverConfig config) {
         try {
-            // String fullClassName = null;
-            // switch (config.getBrowser().toLowerCase()) {
-            //     case "chrome":
-            //         fullClassName = "com.internet.webdriver.selenium.ChromeDriverProvider";
-            //         break;
-            //     case "firefox":
-            //         fullClassName = "com.internet.webdriver.selenium.FirefoxDriverProvider";
-            //         break; 
-            //     case "edge":
-            //         fullClassName = "com.internet.webdriver.selenium.EdgeDriverProvider";
-            //         break;
-            //     default:
-            //         throw new IllegalArgumentException("Unsupported browser: " + config.getBrowser());
-            // }
-            String fullClassName = "com.internet.webdriver.selenium." + config.getBrowser().substring(0, 1).toUpperCase() + config.getBrowser().substring(1).toLowerCase() + "DriverProvider";
+            String fullClassName = null;
+            switch (config.getBrowser().toLowerCase()) {
+                case "chrome":
+                    fullClassName = "com.internet.webdriver.selenium.ChromeDriverProvider";
+                    break;
+                case "firefox":
+                    fullClassName = "com.internet.webdriver.selenium.FirefoxDriverProvider";
+                    break; 
+                case "edge":
+                    fullClassName = "com.internet.webdriver.selenium.EdgeDriverProvider";
+                    break;
+                default:
+                    try {
+                        fullClassName = Files.readString(Path.of("src/test/resources/drivers/driver_class.txt"));
+                    } catch (IOException e) {
+                        throw new RuntimeException("Could not read custom driver class name from file.", e);
+                    }
+            }
             Class<?> clazz = Class.forName(fullClassName);
 
             Constructor<?> cons = clazz.getDeclaredConstructor();
